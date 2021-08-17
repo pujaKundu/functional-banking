83b3e3a2e5f467d8ffd7e9150afd088d162db72c
Puja Kundu <pujakundu797@gmail.com>
2021-08-17 16:57:20 +0600

    balance update

--- js/banking.js
+++ js/banking.js
@@ -14,22 +14,36 @@ function updateTotalField(totalFieldId, newAmount) {
 
 }
 
+function getCurrentBalance() {
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceAmount = parseFloat(balanceTotal.innerText);
+    return previousBalanceAmount;
+}
+
+function updateBalance(newAmount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceAmount = getCurrentBalance();
+    if (isAdd == true) {
+        balanceTotal.innerText = newAmount + previousBalanceAmount;
+    } else {
+        balanceTotal.innerText = previousBalanceAmount - newAmount;
+    }
+
+}
+
 //handle deposit button handle
 document.getElementById('deposit-button')
     .addEventListener('click', function() {
 
         //get input value
         const newDepositAmount = getInputValue('deposit-input');
+        if (newDepositAmount > 0) {
+            //get amount deposited
+            updateTotalField('deposit-total', newDepositAmount);
+            //update account balance
+            updateBalance(newDepositAmount, true);
+        }
 
-        //get amount deposited
-        updateTotalField('deposit-total', newDepositAmount);
-
-        //update account balance
-        const balanceTotal = document.getElementById('balance-total');
-        const previousBalanceAmount = parseFloat(balanceTotal.innerText);
-
-        const newBalanceTotal = newDepositTotal + previousBalanceAmount;
-        balanceTotal.innerText = newBalanceTotal;
 
     })
 
@@ -38,17 +52,15 @@ document.getElementById('withdraw-button').addEventListener(
     'click',
     function() {
 
-        const newWithdrawAmount = getInputValue('withdraw-input')
+        const newWithdrawAmount = getInputValue('withdraw-input');
+        const currentBalance = getCurrentBalance();
+        if (newWithdrawAmount > 0 && newWithdrawAmount < currentBalance) {
             //update withdraw value
-        updateTotalField('withdraw-total', newWithdrawAmount);
-
-
-        //update account balance
-        const balanceTotal = document.getElementById('balance-total');
-        const previousBalanceAmount = parseFloat(balanceTotal.innerText);
-
-        const newBalanceAfterWithdraw = previousBalanceAmount - newWithdrawTotal;
-        balanceTotal.innerText = newBalanceAfterWithdraw;
-        console.log(newBalanceAfterWithdraw)
+            updateTotalField('withdraw-total', newWithdrawAmount);
+            //update account balance
+            updateBalance(newWithdrawAmount, false);
+        } else if (newWithdrawAmount > currentBalance) {
+            console.log('You can not withdraw more than you have in account');
+        }
     }
 )
